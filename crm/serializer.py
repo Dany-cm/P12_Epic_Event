cad4b181b277333828a09de6eee21ba5ba9515da
Dany <danycmwow@gmail.com>
2022-02-05 13:32:07 +0100

    fix: Field payment_due,amount,event_date & attendees validation

--- crm/serializer.py
+++ crm/serializer.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+from rest_framework.exceptions import ValidationError
 from rest_framework.fields import BooleanField, FloatField, IntegerField, CharField
 from rest_framework.serializers import ModelSerializer
 
@@ -14,6 +16,14 @@ class ContractSerializer(ModelSerializer):
     status = BooleanField(required=True)
     amount = FloatField(required=True)
 
+    def validate(self, data):
+        if data['payment_due'] < timezone.now():
+            raise ValidationError({'payment_due': 'Date is invalid'})
+
+        if data['amount'] <= 0:
+            raise ValidationError({'amount': 'Amount cannot be zero or less'})
+        return data
+
     class Meta:
         model = Contract
         fields = '__all__'
@@ -23,6 +33,14 @@ class EventSerializer(ModelSerializer):
     attendees = IntegerField(required=True)
     notes = CharField(required=True)
     support_contact = CharField(read_only=True)
+
+    def validate(self, data):
+        if data['event_date'] < timezone.now():
+            raise ValidationError({'event_date': 'Date is invalid'})
+
+        if data['attendees'] <= 0:
+            raise ValidationError({'attendees': 'Attendees cannot be zero or less'})
+        return data
 
     class Meta:
         model = Event
